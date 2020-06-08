040c5216bb5386dfbecc2aa1130de40067e9ef0a
Luke Taylor <luke.taylor@PALAerospace.com>
2020-06-08 12:39:21 -0230

    atempt to add live map

---
+++ script.js
@@ -0,0 +1,6 @@
+const mymap = L.map('mapID').setView([0, 0], 6);
+const attribution =
+'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(mymap);
